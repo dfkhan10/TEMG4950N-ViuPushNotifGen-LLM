3c2407bb93159d1af47312d671f4efd1cc6ac147
KedahMY <114459232+KedahMY@users.noreply.github.com>
2024-11-06 20:47:38 +0800

    Create slang.py
    Added database list of localised Malay words.

---
+++ backend/utils/slang.py
@@ -0,0 +1,92 @@
+slang_list = [
+    {
+        "word":"kitorang",
+        "meaning":"we people",
+        "explaination":"the short form of the word combination 'kita' and 'orang'",
+        "example":"Kitorang nak pergi makan. Kau nak ikut?",
+    },
+    {
+        "word":"korang",
+        "meaning":"you people",
+        "explaination":"the short form of the word combination 'kau' and 'orang'",
+        "example":"Korang ada nampak Amin tak harini?",
+    },
+    {
+        "word":"Mat Salleh",
+        "meaning":"white person",
+        "explaination":"A colloquial expression used to define a white person",
+        "example":"Jom ajak mat saleh tu minum dekat mamak.",
+    },
+    {
+        "word":"awek",
+        "meaning":"pretty girl",
+        "explaination":"An informal term to call a pretty girl or for a girlfriend",
+        "example":"Siapa perempuan tu? Oh, dia tu awek baru Lukman.",
+    },
+    {
+        "word":"jom",
+        "meaning":"join",
+        "explaination":"similar to 'let's' or 'come on'; can be used to invite someone to do something together",
+        "example":"Jom tengok Kong VS Gorilla hari ini!",
+    },
+    {
+        "word":"Aduh",
+        "meaning":"oww",
+        "explaination":"used to convey the agony, annoyance, and mental pain that someone is giving to you",
+        "example":"Aduh kau ni, aku tunggu kau dua jam tadi.",
+    },
+    {
+        "word":"Fuiyoh",
+        "meaning":"wow",
+        "explaination":"an exaggerated version of WOW, OMG, or wah; when you are tremendously excited or hyped about something",
+        "example":"Fuiyoh, ada orang tu beli iPhone baru lah!",
+    },
+    {
+        "word":"Alamak",
+        "meaning":"Oh no",
+        "explaination":"an exclamation used to symbolize one's dismay, shock, disapproval, or annoyance",
+        "example":"Alamak! Saya tertinggal minit mesyuarat di kedai tadi!",
+    },
+    {
+        "word":"best gila",
+        "meaning":"amazing",
+        "explaination":"used to describe any type of experience in a good way",
+        "example":"Konsert semalam best gila!",
+    },
+    {
+        "word":"mantap",
+        "meaning":"incredible",
+        "explaination":"to describe and express something good, great, amazing, and all other synonymous traits",
+        "example":"Mantap lah persembahan kau tadi!",
+    },
+    {
+        "word":"cincai",
+        "meaning":"careless/sloppy",
+        "explaination":"an adjective used to portray messy or sloppy work or the outcome of something",
+        "example":"Disebabkan kerja dia cincai, dia dapat gred C.",
+    },
+    {
+        "word":"padan muka",
+        "meaning":"serves you right",
+        "explaination":"when someone deserves consequences for a bad action",
+        "example":"Mia kena marah dengan Cikgu Malek sebab meniru. Padan muka dia!",
+    },
+    {
+        "word":"kantoi",
+        "meaning":"busted",
+        "explaination":"when someone gets in trouble",
+        "example":"Habislah kau kantoi. Cikgu Mamat tahu kau ponteng sekolah semalam.",
+    },
+    {
+        "word":"lah",
+        "meaning":"",
+        "explaination":"a versatile suffix used for emphasis or to soften a statement.",
+        "example":"Saya sedang ada sakit kepala lah.",
+    },
+    {
+        "word":"syok",
+        "meaning":"",
+        "explaination":"an adjective to describe something enjoyable or satisfying.",
+        "example":"Syok lah! Chris Martin mendeklamasikan sajak Melayu!",
+    },
+]
