0b68555028fe0474c3f3586ed6c2c6a656f3dfe0
TinaLiXinwei <xlihu@connect.ust.hk>
2024-11-12 15:29:24 +0800

    Add classifier

--- backend/main_no_api.py
+++ backend/main_no_api.py
@@ -5,17 +5,17 @@ load_dotenv(override=True)
 cast = 'KIM Ha Neul'
 viu_datasets = "Viu_datasets"
 
-# # from pipeline.pipeline import castDrivenPipeline
-# # castDrivenPipeline(cast, push_number = 5)
+# from pipeline.pipeline import castDrivenPipeline
+# castDrivenPipeline(cast, push_number = 5)
 
 from pipeline import genPush
 genPush.testingPipeline(cast, push_number = 5)
 
-# # from pipeline.pipeline import contentDrivenPipeline
-# # contentDrivenPipeline('Nothing Uncovered', push_number = 5)
+# from pipeline.pipeline import contentDrivenPipeline
+# contentDrivenPipeline('Nothing Uncovered', push_number = 5)
 
-# # from pipeline import genPush
-# # genPush.testingContentPipeline('Nothing Uncovered', push_number = 5)
+# from pipeline import genPush
+# genPush.testingContentPipeline('Nothing Uncovered', push_number = 5)
 
 # Test for spider
 # from pipeline.getScrapedHolidayItem import run_holiday_spider
