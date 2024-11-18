454857a17dfdf975ae4b92b346bbd8be43aacded
KedahMY <fantasy.kedah@gmail.com>
2024-11-18 22:22:36 +0800

    Added responsiveness to TrendComponent.jsx

--- frontend/src/pages/TrendComponent.jsx
+++ frontend/src/pages/TrendComponent.jsx
@@ -8,7 +8,7 @@ const TrendComponent = ({ title, description, imageSrc }) => {
     };
 
     return (
-        <div className='px-8 py-3'>
+        <div className='px-4 sm:px-2 py-3'>
             <div className="flex items-center bg-[#F5B919] rounded-lg p-4 mb-4 shadow-md">
                 <div 
                     className={`w-4 h-4 border-2 rounded-lg flex items-center justify-center mr-4 cursor-pointer ${checked ? 'bg-gray-800' : 'bg-white'}`} 
@@ -16,8 +16,8 @@ const TrendComponent = ({ title, description, imageSrc }) => {
                 >
                     {checked && <div className="w-2 h-2 bg-white rounded-full" />} {/* Show a small dot when checked */}
                 </div>
-                <img src={imageSrc || './trending.png'} alt={title} className="w-24 h-24 rounded-full mr-4" />
-                <div>
+                <img src={imageSrc || './trending.png'} alt={title} className="flex w-24 h-24 rounded-full mr-4" />
+                <div className='flex-1'>
                     <h3 className="text-lg font-bold">{title}</h3>
                     <p className="text-sm">{description}</p>
                 </div>
